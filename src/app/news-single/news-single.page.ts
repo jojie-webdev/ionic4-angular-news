a32dd3b28910fbd646ee94c5a0fac04f80f7b45c
Jojie Jagonos <jojie.jagonos23@gmail.com>
2019-06-19 17:25:18 +0800

    show data from https://newsapi.org/

--- src/app/news-single/news-single.page.ts
+++ src/app/news-single/news-single.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-news-single',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-single.page.scss'],
 })
 export class NewsSinglePage implements OnInit {
-
-  constructor() { }
+  article;
+  constructor(
+    private newsService: NewsService
+  ) { }
 
   ngOnInit() {
+    this.article = this.newsService.currentArticle;
+    console.log(this.newsService.currentArticle);
   }
 
 }
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { NewsPage } from '../news/news.page';
 
 const routes: Routes = [
   {
@@ -12,7 +13,8 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
+            component: NewsPage
+            // loadChildren: '../tab1/tab1.module#Tab1PageModule'
           }
         ]
       },
